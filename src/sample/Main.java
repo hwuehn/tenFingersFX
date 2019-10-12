45e6ac8b24c1edcd6499bc2081ff9df024d3038a
Henning Wuehn <h.wuehn@posteo.de>
2019-10-12 13:33:56 +0200

    Feature: added content

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,8 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double x, y;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,8 +18,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.DECORATED);
+
+        //we gonna drag the frame
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+
         primaryStage.show();
     }
 }
